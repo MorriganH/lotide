a0fe8b4f1e802b27f074a61484fdacf0a3f23577
MorriganH <morriganhennessy@gmail.com>
2023-04-03 16:41:30 -0400

    returns new array not including first element of inputted array

---
+++ tail.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+/* Test cases
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+const oneElement = [1];
+assertEqual(tail(oneElement).length, 0);
+
+const empty = [];
+assertEqual(tail(empty).length, 0);
+*/
