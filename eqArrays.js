39249bef9c3c6b4a9629310f69731028544a72dd
MorriganH <morriganhennessy@gmail.com>
2023-04-04 02:32:28 -0400

    commented out test cases

--- eqArrays.js
+++ eqArrays.js
@@ -15,6 +15,7 @@ const eqArrays = function(arrayA, arrayB) {
   return true;
 };
 
+/* Test cases
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3, 2], [1, 2, 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 2]), false); // => false
@@ -22,3 +23,4 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+*/
