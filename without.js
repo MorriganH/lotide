a9ed7b8457a53ebae8264cbca477f0b5a49ccb3d
MorriganH <morriganhennessy@gmail.com>
2023-04-04 04:51:21 -0400

    compares source array to a removal array and returns a new array, source but without any values provided in removal

---
+++ without.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed, arrays are equal.`);
+  } else {
+    console.log(`\u274C\u274C\u274C Assertation Failed: arrays are not equal.`);
+  }
+};
+
+const eqArrays = function(arrayA, arrayB) {
+  for (let index = 0; index < arrayA.length || index < arrayB.length; index++) {
+    if (arrayA[index] !== arrayB[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let index = 0; index < source.length; index++) {
+    for (let removeIndex = 0; removeIndex < itemsToRemove.length; removeIndex++) {
+      //if source value same as remove value, check next source value
+      if (source[index] === itemsToRemove[removeIndex]) {
+        break;
+        //if source value has been checked against every remove value and is still ok, add to newArray
+      } else if (removeIndex === itemsToRemove.length - 1) {
+        newArray.push(source[index]);
+      }
+    }
+  }
+  return newArray;
+};
