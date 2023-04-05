09196468fcd472fa14eeee7029728eafa14972b1
MorriganH <morriganhennessy@gmail.com>
2023-04-05 15:22:20 -0400

    bare bones of function and some test cases

---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    return console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed, arrays are equal.`);
+  } 
+  return console.log(`\u274C\u274C\u274C Assertation Failed: arrays are not equal.`);
+};
+
+const eqArrays = function(arrayA, arrayB) {
+  for (let index = 0; index < arrayA.length || index < arrayB.length; index++) {
+    if (arrayA[index] !== arrayB[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  return results;
+};
+
+
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
