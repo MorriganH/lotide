dc8bc107fbee6167509b1a19a8c4742ce79ed5e3
MorriganH <morriganhennessy@gmail.com>
2023-04-05 11:33:30 -0400

    returns an array with the center value(s) of given array

---
+++ middle.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB)) {
+    return console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed, arrays are equal.`);
+  }
+  return console.log(`\u274C\u274C\u274C Assertation Failed: arrays are not equal.`);
+};
+
+const eqArrays = function(arrayA, arrayB) {
+  for (let index = 0; index < arrayA.length || index < arrayB.length; index++) {
+    if (arrayA[index] !== arrayB[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const middle = function(array) {
+  let middleArray = [];
+  if (array.length <= 2) {
+    return middleArray;
+  }
+  if (array.length % 2 === 0) {
+    middleArray.push(array[array.length / 2 - 1]);
+  }
+  middleArray.push(array[Math.floor(array.length / 2)]);
+  console.log(middleArray);
+  return middleArray;
+};
+
+
+/* Test cases
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 5, 3]), [5]);
+assertArraysEqual(middle([1, 2, 5, 4]), [2, 5]);
+assertArraysEqual(middle([1, 2, 6, 4, 5]), [6]);
+assertArraysEqual(middle([1, 2, 10, 1, 5, 6]), [10, 1]);
+*/
