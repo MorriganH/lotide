f86d1c50c8ee848975b0c1fef47e1110569051de
MorriganH <morriganhennessy@gmail.com>
2023-04-05 09:29:44 -0400

    removed use of else keyword in assertEqual and assertArraysEqual used in all files

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,6 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
+    return console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed: ${actual} === ${expected}`);
   }
+  return console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
 };
--- tail.js
+++ tail.js
@@ -1,9 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
+    return console.log(`\u{1F4B9}\u{1F4B9}\u{1F4B9} Assertation Passed: ${actual} === ${expected}`);
   }
+  return console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
 };
 
 const tail = function(array) {
