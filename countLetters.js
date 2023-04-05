8e289e9367ae40d875dfebf1345cfa8db691d73f
MorriganH <morriganhennessy@gmail.com>
2023-04-05 15:08:33 -0400

    adds count of each letter to an object, ignoring spaces

--- countLetters.js
+++ countLetters.js
@@ -7,5 +7,29 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(string) {
   let counts = {};
-  
-}
+  for (let character of string) {
+    if (character !== " ") {
+      if (counts[character]) {
+        counts[character]++;
+      } else {
+        counts[character] = 1;
+      }
+    }
+  }
+  console.log(counts);
+  return counts;
+};
+
+/* Test cases
+const testString1 = "lighthouse in the house";
+const testString2 = "supercallafrajulisticexpialladocious";
+
+assertEqual(countLetters(testString1).h, 4);
+assertEqual(countLetters(testString1).l, 1);
+assertEqual(countLetters(testString1).t, 2);
+assertEqual(countLetters(testString1).z, undefined);
+assertEqual(countLetters(testString2).s, 3);
+assertEqual(countLetters(testString2).l, 5);
+assertEqual(countLetters(testString2).x, 1);
+assertEqual(countLetters(testString2).z, undefined);
+*/
