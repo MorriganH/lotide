a6f3208fe639873ec30befecf4a07d9880170f08
MorriganH <morriganhennessy@gmail.com>
2023-04-05 16:35:05 -0400

    adds for in loop to check all keys in object and compares them to the value inputted, returns the key name that matches

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,12 +5,18 @@ const assertEqual = function(actual, expected) {
   return console.log(`\u274C\u274C\u274C Assertation Failed: ${actual} !== ${expected}`);
 };
 
-const findKeyByValue = function(inObject, value) {
+const findKeyByValue = function(object, value) {
   let foundKey;
-
+  for (let key in object) {
+    if (object[key] === value) {
+      foundKey = key;
+    }
+  }
   return foundKey;
 };
 
+
+/* Test cases
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
@@ -18,4 +24,7 @@ const bestTVShowsByGenre = {
 };
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+*/
